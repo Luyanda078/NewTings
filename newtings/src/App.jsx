09a88407b3108e1c88db89a65f8eb1e53d8face1
Luyanda <luyandashamase5@gmail.com>
2025-01-10 13:20:44 +0200

    Feat: Portfolio updated

--- newtings/src/App.jsx
+++ newtings/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Hero from './components/hero/Hero'
+import Skills from './components/skills/Skills';
+import Experience from './components/experience/Experience';
+import Language from './components/language/Language';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  useEffect(()=>{
+    AOS.init({
+      duration: 1000,
+    });
+  },[]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <main className='bg-[#0d182e]'>
+    <Hero /> 
+    <Skills />
+    <Experience />
+    <Language />
+    <Projects />
+    <Contact />
+    <Footer />
+   </main>
   )
 }
-
-export default App
